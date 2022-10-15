b934904d6138dcb32ddc542865315c5e59fb0961
ABDUL <104834399+Abdulbasithrahman@users.noreply.github.com>
2022-10-15 14:18:19 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": '99e080b24dmshba31c7c58c3ef99p1361f4jsn8e74a7a87879',
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+  export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+
+  export const WEATHER_API_KEY="f5a4c31b9178218b79d84237f70fd20e";
